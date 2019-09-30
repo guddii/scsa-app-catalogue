72388800e52df2795a17caa7283590478eece5b1
Florian Gudat <florian.gudat@guices.com>
2019-09-30 12:08:41 +0200

    Added lerna support

--- client/src/catalogue.ts
+++ client/src/catalogue.ts
@@ -1,6 +1,6 @@
-import "./main.css";
+import "./catalogue.css";
 
-import { SecurityChecks } from "scsa-lib-messaging/src/SecurityChecks";
+import { SecurityChecks } from "@scsa/messaging/src/SecurityChecks";
 import { Applications } from "./scripts/Constant";
 import { App } from "./scripts/App";
 
